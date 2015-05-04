7d5af773ccbb9a014a82ee365af8351b6fe491ea
dskrypa <dskrypa@gmail.com>
2015-05-04 18:04:58 -0400

    laid framework for ls options

--- ls/myls.c
+++ ls/myls.c
@@ -9,15 +9,14 @@
 
 #include <stdio.h>					//IO functions
 #include <stdlib.h>					//Memory functions; exit()
+#include <unistd.h>					//Misc symbolic constants, types, and functions
 #include <errno.h>					//Access to stderr stream
 #include <dirent.h>					//Directory access
 
-//TEMP
-#include <string.h>					//String functions
-#include <ctype.h>					//Character conversions and testing
-
 #include "modernize.h"				//Defines bool & String
-//#include "stringUtils.h"			//String comparison & manipulation
+#include "stringUtils.h"			//String comparison & manipulation
+
+#include "myls.h"
 
 /*
 ls			list the names of files and directories in the current directory
@@ -37,38 +36,92 @@ lstat()			obtain file information
 ftsat()			obtain file information
 */
 
-void plain(String);
-
 int main(int argc, String* argv) {
-	DIR* dir;
-	struct dirent* dent;
-	
 	unsigned cwd_buffer = 4096;
 	char cwd[cwd_buffer];
 	getcwd(cwd,cwd_buffer);
 	
-	plain(cwd);
+	if (argc == 1) {
+		ls_plain(cwd);
+	} else if (argc == 2) {
+		fprintf(stderr,"Error: args not yet supported.\n");
+	} else {
+		fprintf(stderr,"Error: multiple args not yet supported.\n");
+	}
+	
 	return 1;
 }
 
-void plain(String dir_name) {
+DIR* getDir(String path) {
 	DIR* dir;
-	struct dirent* dent;
-	
-	if ((dir = opendir(dir_name)) == NULL) {
-		fprintf(stderr,"Error while opening directory: %s\n", dir_name);
+	if ((dir = opendir(path)) == NULL) {
+		fprintf(stderr, "Error while opening directory: %s\n", path);
 		exit(0);
 	}
+	return dir;
+}
+
+/**
+	List the names of files and directories in the current directory.
+	Note: real ls starts line wrapping with 14+ files; left-aligns columns
+	@param path the full path of the directory to examine
+*/
+void ls_plain(String path) {
+	DIR* dir = getDir(path);
 	
-	//Note: real ls starts line wrapping with 14+ files; left-aligns columns
+	struct dirent* dent;	
 	while ((dent = readdir(dir)) != NULL) {										//Iterate through all items in the current directory
-		if ((strcmp(".", dent->d_name)!=0) && (strcmp("..", dent->d_name)!=0)) {
+		if (!streq(".", dent->d_name) && !streq("..", dent->d_name)) {
 			printf("%s  ", dent->d_name);										//If the file name is not . or .., then print it
 		}
 	}
+	printf("\n");
 	
 	if (closedir(dir) == -1) {
-		fprintf(stderr,"Encountered error while closing %s\n", dir_name);
+		fprintf(stderr,"Encountered error while closing %s\n", path);
+		exit(0);
+	}
+}
+
+/**
+	List the names and i-node numbers of files and directories in the current
+	directory.
+	@param path the full path of the directory to examine
+*/
+void ls_i(String path) {
+	DIR* dir = getDir(path);
+	
+	if (closedir(dir) == -1) {
+		fprintf(stderr,"Encountered error while closing %s\n", path);
+		exit(0);
+	}
+}
+
+/**
+	List the names of files and directories in long format.
+	@param path the full path of the directory to examine
+*/
+void ls_l(String path) {
+	DIR* dir = getDir(path);
+	
+	if (closedir(dir) == -1) {
+		fprintf(stderr,"Encountered error while closing %s\n", path);
+		exit(0);
+	}
+}
+
+/**
+	Recursively list the names of all files and directories in the current
+	directory; that is, the command is also applied to the directories within
+	the current directory, the directories within those directories, and so
+	forth.
+	@param path the full path of the directory to examine
+*/
+void ls_R(String path) {
+	DIR* dir = getDir(path);
+	
+	if (closedir(dir) == -1) {
+		fprintf(stderr,"Encountered error while closing %s\n", path);
 		exit(0);
 	}
 }
---
+++ ls/myls.h
@@ -0,0 +1,12 @@
+/**
+ * Douglas Skrypa
+ * Version: 2015.05.04
+ * Homework 6: Implementing ls
+ */
+
+DIR* getDir(String path);
+void ls_plain(String path);
+void ls_i(String path);
+void ls_l(String path);
+void ls_R(String path);
+
